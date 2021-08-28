4d157de6abcb44528161ee2b61dcee9aba75307a
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-08-28 17:30:51 -0600

    commit tree bbb9ae3753590c9dc6549fb831e4bc8f403e6280

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching (drivers, str) {
+    return drivers.filter (driver => driver.toLowerCase() === str.toLowerCase())
+}
+function fuzzyMatch (drivers, str) {
+    const strLength = str.length
+    return drivers.filter (driver => driver.slice(0,strLength) === str)
+}
+function matchName (drivers, str) {
+    return drivers.filter (driver => driver.name === str)
+}
